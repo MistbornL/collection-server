158b5630e0295ccca0469ec7ad1b04b9815cc75d
MistbornL <lashaloria3@gmail.com>
2022-10-23 15:23:05 +0400

    googleauth

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,93 +12,131 @@ const SECRET = process.env.SECRET;
 
 // Signup route to create a new user
 router.post("/signup", async (req, res) => {
-  try {
-    // hash the password
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-    const candidate = await User.findOne({ email: req.body.email });
-    if (candidate) {
-      res.status(409).json({ error: "email already exists" });
-    } else {
-      const user = await User.create(req.body);
-      res.json(user);
+  if (req.body.googleAccessToken) {
+    // gogole-auth
+    const { googleAccessToken } = req.body;
+
+    axios
+      .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+          Authorization: `Bearer ${googleAccessToken}`,
+        },
+      })
+      .then(async (response) => {
+        const firstName = response.data.given_name;
+        const lastName = response.data.family_name;
+        const email = response.data.email;
+        const picture = response.data.picture;
+
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser)
+          return res.status(404).json({ message: "User don't exist!" });
+
+        const token = jwt.sign(
+          {
+            email: existingUser.email,
+            id: existingUser._id,
+          },
+          config.get("JWT_SECRET"),
+          { expiresIn: "1h" }
+        );
+
+        res.status(200).json({ result: existingUser, token });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: "Invalid access token!" });
+      });
+  } else {
+    try {
+      // hash the password
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+      const candidate = await User.findOne({ email: req.body.email });
+      if (candidate) {
+        res.status(409).json({ error: "email already exists" });
+      } else {
+        const user = await User.create(req.body);
+        res.json(user);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ error });
   }
 });
 
 // Login route to verify a user and get a token
 router.post("/login", async (req, res) => {
-  // if (req.body.googleAccessToken) {
-  //   // gogole-auth
-  //   const { googleAccessToken } = req.body;
+  if (req.body.googleAccessToken) {
+    // gogole-auth
+    const { googleAccessToken } = req.body;
 
-  //   axios
-  //     .get("https://www.googleapis.com/oauth2/v3/userinfo", {
-  //       headers: {
-  //         Authorization: `Bearer ${googleAccessToken}`,
-  //       },
-  //     })
-  //     .then(async (response) => {
-  //       const firstName = response.data.given_name;
-  //       const lastName = response.data.family_name;
-  //       const email = response.data.email;
-  //       const picture = response.data.picture;
+    axios
+      .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+          Authorization: `Bearer ${googleAccessToken}`,
+        },
+      })
+      .then(async (response) => {
+        const firstName = response.data.given_name;
+        const lastName = response.data.family_name;
+        const email = response.data.email;
+        const picture = response.data.picture;
 
-  //       const existingUser = await User.findOne({ email });
+        const existingUser = await User.findOne({ email });
 
-  //       if (!existingUser)
-  //         return res.status(404).json({ message: "User don't exist!" });
+        if (!existingUser)
+          return res.status(404).json({ message: "User don't exist!" });
 
-  //       const token = jwt.sign(
-  //         {
-  //           email: existingUser.email,
-  //           id: existingUser._id,
-  //         },
-  //         config.get("JWT_SECRET"),
-  //         { expiresIn: "1h" }
-  //       );
+        const token = jwt.sign(
+          {
+            email: existingUser.email,
+            id: existingUser._id,
+          },
+          config.get("JWT_SECRET"),
+          { expiresIn: "1h" }
+        );
 
-  //       res.status(200).json({ result: existingUser, token });
-  //     })
-  //     .catch((err) => {
-  //       res.status(400).json({ message: "Invalid access token!" });
-  //     });
-  // } else
-  try {
-    // check if the user exists
-    var user = await User.findOne({ email: req.body.email });
-    var date = new Date();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    var year = date.getFullYear();
-    var newDate = year + "/" + month + "/" + day;
+        res.status(200).json({ result: existingUser, token });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: "Invalid access token!" });
+      });
+  } else {
+    try {
+      // check if the user exists
+      var user = await User.findOne({ email: req.body.email });
+      var date = new Date();
+      var month = date.getMonth() + 1;
+      var day = date.getDate();
+      var year = date.getFullYear();
+      var newDate = year + "/" + month + "/" + day;
 
-    if (user.status === "Blocked") {
-      return res.status(403).json({ message: "This email is blocked." });
-    }
-
-    if (user) {
-      //check if password matches
-      const result = await bcrypt.compare(req.body.password, user.password);
-      if (result) {
-        // sign token and send it in response
-        const token = await jwt.sign({ email: user.email }, SECRET, {
-          expiresIn: 60 * 60,
-        });
-        user.dateLastAuthorization = newDate;
-        user.status = "Online";
-        await user.save();
-        res.json({ user, token, userId: user.id });
-      } else {
-        res.status(401).json({ error: "password doesn't match" });
+      if (user.status === "Blocked") {
+        return res.status(403).json({ message: "This email is blocked." });
       }
-    } else {
-      res.status(404).json({ error: "User doesn't exist" });
+
+      if (user) {
+        //check if password matches
+        const result = await bcrypt.compare(req.body.password, user.password);
+        if (result) {
+          // sign token and send it in response
+          const token = await jwt.sign({ email: user.email }, SECRET, {
+            expiresIn: 60 * 60,
+          });
+          user.dateLastAuthorization = newDate;
+          user.status = "Online";
+          await user.save();
+          res.json({ user, token, userId: user.id });
+        } else {
+          res.status(401).json({ error: "password doesn't match" });
+        }
+      } else {
+        res.status(404).json({ error: "User doesn't exist" });
+      }
+    } catch (error) {
+      res.status(400).json({ error });
     }
-  } catch (error) {
-    res.status(400).json({ error });
   }
 });
 
--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
   role: { type: String, required: true, default: "user" },
   status: { type: String, required: true, default: "Active" },
   dateLastAuthorization: { type: Date },
